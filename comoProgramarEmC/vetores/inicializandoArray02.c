97a158c13a3bbf9f182774cfd6ab9db32c09a5ae
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-21 16:57:30 -0300

    Update inicializandoArray02.c

--- comoProgramarEmC/vetores/inicializandoArray02.c
+++ comoProgramarEmC/vetores/inicializandoArray02.c
@@ -9,6 +9,19 @@ int main()
 
     system("cls"); // limpa a tela
 
+    // vetor
+    int n[10] = {1, 2, 5, 6, 9, 8, 7, 4, 3, 10};
+
+    // variável
+    int i;
+
+    printf("%s%13s\n", "Elemento", "Valor");
+
+    // loop for para exibir os valores do vetor
+    for(i = 0; i < 10; i++) {
+      printf("%5d%13d\n", i, n[i]);
+    } // end for
+
     printf( "Olá, Mundo C!\n"); // imprime na tela e pula uma linha
 
     system("pause"); // pausa do programa
