49ba52de39bd8f623a31ba95f808247190d03130
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-22 11:30:51 -0300

    Create inteirosPares03.c

---
+++ comoProgramarEmC/vetores/inteirosPares03.c
@@ -0,0 +1,36 @@
+// incluir biblioteca
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define SIZE 10
+
+// função principal
+int main()
+{
+    setlocale( LC_ALL, "portuguese"); // localização geográfica
+
+    system("cls"); // limpa a tela
+    
+    // vetor
+    int vetor[SIZE];
+    
+    // VARIÁVEL
+    int i;
+    
+    // inicializa vetor
+    for(i = 2; i <= 20; i += 2) {
+      vetor[i] = i;
+    } // end for 
+    
+    printf("%s\n", "Valores:");
+    
+    // loop para exibir
+    for(i = 2; i <= 20; i += 2) {
+      printf( "%5d\n", vetor[i]); // imprime na tela e pula uma linha
+    } // end for
+    
+    system("pause"); // pausa do programa
+
+    return 0; // programa terminado com sucesso
+
+} // fim main
