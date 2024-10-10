11683fc6a34c7d580308c71a3b727548545d51d0
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-10 15:01:09 -0300

    Update string01.c

--- wagnerGaspar/string/exemplos/string01.c
+++ wagnerGaspar/string/exemplos/string01.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * Uma String é uma sequência de caracteres
+ * String na linquagem C é um vetor do tipo char
+ * caracteres, por exemplo:
+ * cria um vetor para 100 caracteres
+ * char vetor[100] = {"Tudo entre aspas duplas é uma string."};
+ */
 
 int main()
-{
-   // variável
-   char palavras[]
-   printf("");
-}
+{   // 1º) cria um vetor para armazenar uma 
+   // sequência de caracteres
+   // Um vetor do tipo char com nome palavras 
+   // recebe uma string entre aspas duplas
+   char palavras[] = ("\nOi. Vamos aprender a programar com a linguagem C?\n\n");
+
+   // exibe o conteúdo do vetor palavras
+   printf("%s", palavras);
+
+   return 0;
+
+} // end main
