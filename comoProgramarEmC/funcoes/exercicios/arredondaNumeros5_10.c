719291eccbcfc2c78f29a587ec6d7cc4857c08f7
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-12 17:54:33 -0300

    Update arredondaNumeros5_10.c

--- comoProgramarEmC/funcoes/exercicios/arredondaNumeros5_10.c
+++ comoProgramarEmC/funcoes/exercicios/arredondaNumeros5_10.c
@@ -1,5 +1,17 @@
 /**
- * Exercício 5.10 Arredondando números.
+ * 5.10 Arredondando números. Uma das aplicações da 
+ * fun-  ção floor é arredondar um valor para o 
+ * inteiro mais  próximo. 
+ * A instrução  y = floor( x + .
+ * 5);  arredondará o número x para o inteiro mais 
+ * próximo e  atribuirá o resultado a y. 
+ * Escreva um programa que leia  vários números e use 
+ * o comando anterior para arredondar cada um desses 
+ * números para o inteiro mais próximo. Para cada 
+ * número processado, imprima o número  original e o 
+ * número arredondado. 
+ * Deitel, Paul; Deitel, Harvey. C Como Programar 
+ * (Portuguese Edition) (p. 153). Edição do Kindle. 
  * Deitel, Paul; Deitel, Harvey. C  Como
  * Programar (Página 153). Edição do Kindle.
  * Pedro Filho, 12/10/2024
