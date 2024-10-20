6d0c003b9ce98994fb75ddf441497223a87f81cf
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-20 12:03:38 -0300

    Create comentariosEmC01.c

---
+++ wagnerGaspar/comentarios/comentariosEmC01.c
@@ -0,0 +1,26 @@
+/**
+ *  (barra/ asterisco * abre o comentario de varias 
+ *                         linhas.
+ *    * asterisco / barra fecha o comentário de varias 
+ *                         linhas.)
+ */
+
+// barra barra é um comentario de um linha
+
+#include <stdio.h> // biblioteca da linquagem C
+
+// função principal
+int main()
+{ // abre o bloco de código
+   
+   // função para imprimir algo na tela
+   printf("funcao para exibir algo na tela\n\n");
+
+   /*
+      este é
+      um comentário 
+      várias linhas. 
+   */
+   return 0; // informa fim do programa.
+
+} // fecha o bloco de código
