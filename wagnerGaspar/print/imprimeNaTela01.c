0531e69f41b3a56d59ff1d07182bd937f292425d
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-20 12:03:45 -0300

    Create imprimeNaTela01.c

---
+++ wagnerGaspar/print/imprimeNaTela01.c
@@ -0,0 +1,17 @@
+/**
+ *  printf(); função para imprimir algo na tela
+ */
+
+#include <stdio.h> // biblioteca da linquagem C
+
+// função principal
+int main()
+{ // abre o bloco de código
+   
+   // função para imprimir algo na tela
+   printf("funcao para exibir algo na tela\n\n");
+   // o que está entre as aspas "será exibido na tela"
+
+   return 0; // informa fim do programa.
+
+} // fecha o bloco de código
