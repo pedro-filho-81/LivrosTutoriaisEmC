959c4a3f74a1b6a5a80f1cccbb1ce99b5f7899c6
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-23 12:03:52 -0300

    Update imprimeNaTela01.c

--- wagnerGaspar/print/imprimeNaTela01.c
+++ wagnerGaspar/print/imprimeNaTela01.c
@@ -1,16 +1,19 @@
 /**
  *  printf(); função para imprimir algo na tela
  */
+// biblioteca obrigatória da linquagem C
+#include <stdio.h> 
 
-#include <stdio.h> // biblioteca da linquagem C
-
-// função principal
+// int main() função principal
+// int => tipo da função
+// main => nome da função
+// () diz ao compilador que main é uma função
 int main()
 { // abre o bloco de código
    
    // função para imprimir algo na tela
    printf("funcao para exibir algo na tela\n\n");
-   // o que está entre as aspas "será exibido na tela"
+   // aqui o que está entre as aspas "será exibido na tela"
 
    return 0; // informa fim do programa.
 
