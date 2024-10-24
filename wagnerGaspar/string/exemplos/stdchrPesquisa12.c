24db56496ff603da0e84e34cbc6d6d02b7480794
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-24 16:52:30 -0300

    Update stdchrPesquisa12.c

--- wagnerGaspar/string/exemplos/stdchrPesquisa12.c
+++ wagnerGaspar/string/exemplos/stdchrPesquisa12.c
@@ -1,23 +1,33 @@
 /**
- * A função strchr() retorna um ponteiro para a 
+ * A função strchr() retorna o endereço para um ponteiro para a 
  * posição da primeira ocorrência de um caractere 
  * em uma string.
  * A função strchr() é definida na biblioteca <string.h>.
+ * 
  */
+
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
    // cria string
-   char palavra[] = {"Hello World"};
+   char palavra[] = {"abacate"};
    // cria ponteiro
-   char *charPtr = strchr(palavra, 'W');
+   char *ptrPalavra;
 
-   // verifica se a letra
-   if (charPtr != NULL)
-   {
-      // exibe palavra
-      printf("\nPalavra %s encontrada.\n\n", charPtr);
-   } // end if   
+   // ptrPalavra recebe o endereço da primeira ocorrência da letra a
+   ptrPalavra = strchr(palavra, 'a');
+
+   printf("\nDeclara, cria e inicializa a String:");
+   printf("\nchar palavra[] = {\"abacate\"};");
+   printf("\nDeclara ponteiro:\nchar *ptrPalavra;");
+   printf("\nptrPalavra recebe o endereco retornado por strchr()");
+   printf("\nptrPalavra = strchr(palavra, 'a');");
+   printf("\nO endereco recebido por ptrPalavra eh: %p\n", ptrPalavra);
+   printf("O endereco de:\nchar palavra[] = {\"abacate\"}; eh: %p\n", &palavra);
+
+   printf("\n"); // pula linha
+   return 0;
+
 } // end main
