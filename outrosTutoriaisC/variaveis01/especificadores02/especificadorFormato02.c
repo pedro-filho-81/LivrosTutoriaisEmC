db78d55ccb65a1c655f8d7a38da4dc2669be9cea
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-24 11:49:23 -0300

    Update especificadorFormato02.c

--- outrosTutoriaisC/variaveis01/especificadores02/especificadorFormato02.c
+++ outrosTutoriaisC/variaveis01/especificadores02/especificadorFormato02.c
@@ -1,15 +1,20 @@
 /**
+ * declara e cria um variável
+ * int variavel = 25;
  * Especificadores de formato são usados juntos 
- * com a função printf() por exemplo, para exebir
- * o valor de uma variável inteira usa-se o especificador
- * (%d) entre aspas duplas ("%d") dentro da função
- * printf("%d"); (;) encerra a instrução
+ * com a função printf() por exemplo, para exibir
+ * o valor de uma variável inteira na tela 
+ * usa-se o especificador (%d) 
+ * entre aspas duplas ("%d") 
+ * dentro da função printf("%d", variável); 
+ * essa função exibi o valor 25 na tela
+ * o ponto e vírgula (;) encerra a instrução.
  */
 #include <stdio.h>
 
 int main()
 {
-   // variável
+   // variável declara e cria
    int a = 25;
 
    // exibe o valor da variável a
