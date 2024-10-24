2c7fc3af5ded3aeac5b20b0b047bb20bd42be8f0
PedroSantosFilho <psfilho81@hotmail.com>
2024-10-24 11:49:40 -0300

    Create declaraCriaInicializa01.c

---
+++ outrosTutoriaisC/arrays/declaraCriaInicializa01.c
@@ -0,0 +1,12 @@
+/**
+ * declara, cria e inicializa um vetor
+ * para criar um vetor primeiro define-se 
+ * o tipo (int, char, float) (int) num[];
+ * segundo especifica-se um nome int (num)[] 
+ * terceiro coloca-se cochetes[] a frente
+ * do nome int nomeDoArray([]). Para inserir
+ * valores usa-se uma lista de valores separado
+ * por vírgula entre chaves {valores}
+ * int nomeDoArray[] = {1, 2, 3, 4, 5};
+ * todos os valores devem ser do meso tipo.
+ */
