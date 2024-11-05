6b580875d7378beabea1921637e335855391c75f
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-05 11:54:42 -0300

    Create vendasDaLoja12.c

---
+++ wagnerGaspar/string/exercicios/vendasDaLoja12.c
@@ -0,0 +1,18 @@
+/**
+ * 12) Escreva um programa que leia o nome e o valor 
+ * de determinada mercadoria de uma loja. Sabendo que 
+ * o desconto para pagamento à vista é de 10% sobre o 
+ * valor total, calcule o valor a ser pago à vista. 
+ * Escreva o nome da mercadoria, o valor total, o 
+ * valor do desconto e o valor a ser pago à vista.
+ * Backes, André. Linguagem C - Completa e 
+ * Descomplicada (Portuguese Edition) (p. 140). 
+ * GEN LTC. Edição do Kindle. 
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+   return 0;
+} // end main
