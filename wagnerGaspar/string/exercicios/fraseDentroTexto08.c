a14e05b673b939f39cc2ea5cb6508c5bf8ec4e1e
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-05 11:54:23 -0300

    Update fraseDentroTexto08.c

--- wagnerGaspar/string/exercicios/fraseDentroTexto08.c
+++ wagnerGaspar/string/exercicios/fraseDentroTexto08.c
@@ -1,7 +1,6 @@
 /**
- * strcmp() Compara os valores ASCII de caracteres em 
- * duas strings para determinar qual string tem um valor 
- * mais alto
+ * A função strstr() retorna um ponteiro para a posição da 
+ * primeira ocorrência de uma string em outra string.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,30 +12,47 @@ int main()
    char texto[50];
    char frase[50];
    char *resposta; // para função strstr(texto, pesquisa)
+   char retorno = 's';
+   int i = 0;
 
-   printf("\nPESQIOSA PALAVRA NO TEXTO\n");
-   
-   // data entry
-   printf("Digite um texto: ");
-   scanf(" %50[^\n]", texto);
+   while (retorno == 's')
+   {   
+      printf("\nPESQIOSA PALAVRA NO TEXTO\n");
 
-   printf("Digite uma frase ou uma \npalavra a pesquisar: ");
-   scanf(" %50[^\n]", frase);
+      // data entry
+      printf("Digite um texto: ");
+      scanf(" %50[^\n]", texto);
 
-   // ponteiro resposta recebe o endereço da pesquisa
-   // ou NULL se frase/palavra não estiver contida no texto
-   resposta = strstr(texto, frase);
+      printf("Digite uma frase ou uma \npalavra a pesquisar: ");
+      scanf(" %50[^\n]", frase);
 
-   // se a resposta for diferente de nula
-   if (resposta != NULL) // se verdade
-   {
-      // exibe
-      printf("A frase/palavra %s esta contida na frase \n%s\n\n", frase, texto);
-   } // end if
-   else { // se falso
-      // exibe
-      printf("A frase/palavra %s\nNao Esta contida no texto: %s\n\n", frase, texto);
-   } // end else
+      // ponteiro resposta recebe o endereço da pesquisa
+      // ou NULL se frase/palavra não estiver contida no texto
+      resposta = strstr(texto, frase);
+
+      // se a resposta for diferente de nula
+      if (resposta != NULL) // se verdade
+      {
+         // exibe
+         printf("A frase/palavra %s esta contida na frase \n%s\n\n", frase, resposta);
+
+      } // end if
+      else { // se falso
+         // exibe
+         printf("A frase/palavra %s\nNao Esta contida no texto: %s\n\n", frase, resposta);
+      } // end else
+
+      // data entry   
+      printf("\nDeseja continuar? (s/n): ");
+      scanf(" %c", &retorno); // entrada do usuário
+
+      // se retorno igual a n
+      if (retorno == 'n')
+      {
+         // sair do programa
+         break;
+      } // end if sair
+   } // end while
 
    return 0; // programa okay
 
