bb62390560cfc27f44043852c0453f914b779b48
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-05 17:42:18 -0300

    Update vendasDaLoja12.c

--- wagnerGaspar/string/exercicios/vendasDaLoja12.c
+++ wagnerGaspar/string/exercicios/vendasDaLoja12.c
@@ -9,10 +9,75 @@
  * Descomplicada (Portuguese Edition) (p. 140). 
  * GEN LTC. Edição do Kindle. 
  */
+
 #include <stdio.h>
 #include <ctype.h>
 
 int main()
 {
+   // strings
+   char nomeProd[3][50]; // matriz de string
+   char cadastrar = 's';
+
+   int quantidade[20];
+   int i = 0; // para loops
+   
+   float vlProduto[20];
+   float vlPagamento[20];
+   float vlTotal;
+   float vlDescontos;
+
+   printf("\nCADASTRO DAS VENDAS.\n");
+
+   // loop para entrada dos dados
+   while (cadastrar == 's')  
+   {
+      // data entry
+      printf("Digite o nome do produto: ");
+      scanf(" %30[^\n]", nomeProd[i]);
+      printf("Digite a quantidade: ");
+      scanf("%d", &quantidade[i]);
+      printf("Valor do produto: R$ ");
+      scanf("%f", &vlProduto[i]);
+
+      // calcular
+      vlPagamento[i] = vlProduto[i] * quantidade[i];
+
+      vlTotal += vlPagamento[i];
+      
+      printf("Deseja fazer novo cadastro? (s/n): ");
+      scanf(" %c", &cadastrar);
+      
+      // se cadastrar igual a não (n)
+      if (cadastrar == 'n')
+      {
+         break; // sair do cadastro
+      } // end if
+      
+      i++; // incrementa os índices
+
+   } // end for entrada
+
+   // loop para exibir as compras
+   for ( i = 0; i < 3; i++)
+   {
+      // exibir resultado
+      printf("\nProduto: %s\nQuantudade: %d\nVlProduto: R$ %.2f\n",nomeProd[i], quantidade[i], vlProduto[i]);
+
+      printf("Valor a pagar R$ %.2f\n", vlPagamento[i]);
+
+   } // end for
+   
+   // calcular descontos
+   vlDescontos = vlTotal * 0.10;
+
+   printf("\nPAGAmENTO\n");
+
+   // exibe valor total
+   printf("\nTotal a pagar R$ %.2f\n", vlTotal);
+   printf("Avista tem R$ %.2f de descontos.\n", vlDescontos);
+   printf("Total avista R$ %.2f\n\n", vlTotal - vlDescontos);
+
    return 0;
+
 } // end main
