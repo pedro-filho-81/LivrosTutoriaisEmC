5d254f46740aa3b87eeca4886d8c1d9f3174e125
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-07 15:16:53 -0300

    Create codigoDeCesar14.c

---
+++ wagnerGaspar/string/exercicios/codigoDeCesar14.c
@@ -0,0 +1,26 @@
+/**
+ * 14) O código de César é uma das técnicas de 
+ * criptografia mais simples e conhecidas. É um tipo 
+ * de substituição no qual cada letra do texto é 
+ * substituída por outra, que se apresenta n posições 
+ * após ela no alfabeto. Por exemplo, com uma troca 
+ * de três posições, a letra A seria substituída por 
+ * D, B se tornaria E e assim por diante. 
+ * Escreva um programa que faça uso desse código de 
+ * César para três posições. Entre com uma string e 
+ * imprima a string codificada. Exemplo: String: a 
+ * ligeira raposa marrom saltou sobre o cachorro 
+ * cansado. Nova string: d oljhlud udsrvd pduurp 
+ * vdowrx vreuh r fdfkruur fdqvdgr
+ * Backes, André. Linguagem C - Completa e 
+ * Descomplicada (Portuguese Edition) (p. 140). 
+ * GEN LTC. Edição do Kindle. 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+} // end main
