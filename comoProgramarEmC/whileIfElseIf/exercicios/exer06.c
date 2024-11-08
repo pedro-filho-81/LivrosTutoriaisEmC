ee9b1eab4161b21b0162c452933102782e1a22f7
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-08 17:33:08 -0300

    Create exer06.c

---
+++ comoProgramarEmC/whileIfElseIf/exercicios/exer06.c
@@ -0,0 +1,23 @@
+/**
+ * 
+ * 08/10/2024
+ */
+
+// inclua bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+// Função principal
+int main()
+{ // Inicio de main
+
+    // Variáveis
+    int produto, x = 5;
+
+    // imprime na tela
+    printf("\no resultado de produto *= x++ igual %d\n", produto *= x++);
+    // resultado = 40.
+    
+    return 0; // programa ok
+    
+} // fim de main
