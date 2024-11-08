dc607c40922cb1525462922393d5d262409d207b
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-08 17:33:30 -0300

    Create tamanhoDaString02.c

---
+++ wagnerGaspar/funcao/exemplos/tamanhoDaString02.c
@@ -0,0 +1,50 @@
+/**
+ * Cria uma função que retorna o tamanho da string
+ */
+#include <stdio.h>
+#include <string.h>
+
+// cria a função para contar a quantidade 
+// de caracteres da string
+// int => tipo de retorno da função inteiro
+// minhaStrlen => nome da função
+// toda função tem parêntese ()
+// dentro dos parênteses(tem, ou não tem parâmetros)
+// os parâmetros recebem argumentos
+int minhaStrlen(char str[])
+{
+   // variável
+   int tam = 0; // para contar os caracteres da string
+
+   // enquanto não chegar ao final da string faça
+   while (str[tam] != '\0')
+   {
+      // conta os caracteres da string
+      tam++; // incrementa 1 a tam
+   } // end while   
+
+   // retorna o valor de tam
+   return tam;
+
+} // end minhaStrlen
+
+int main()
+{
+   // string
+   char vet[20] = {"Ola. Bom dia!"};
+
+   printf("\nEXIBIR O TAmANHO DA STRING\n");
+   // exibe a string
+   printf("String: %s\n", vet);
+
+   // exibe o tamanho da string com a função strlen
+   // da biblioteca string.h
+   printf("strlen: %d\n", strlen(vet));
+   
+   // exibe o tamanho da string com a função 
+   // minhaStrlen() criada pelo programador
+   printf("minhaStrlen: %d\n\n", minhaStrlen(vet));
+
+   return 0; // final do prog
+
+} // end main
