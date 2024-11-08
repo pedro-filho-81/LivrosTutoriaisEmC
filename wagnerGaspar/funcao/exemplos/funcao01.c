eb88dd8781612a07b096de9793662d9b32d2b31a
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-08 17:32:56 -0300

    Update funcao01.c

--- wagnerGaspar/funcao/exemplos/funcao01.c
+++ wagnerGaspar/funcao/exemplos/funcao01.c
@@ -14,7 +14,10 @@ int main()
    tam = printf("Hello");
    printf("\nTamanho da string: %d\n", tam);
 
+   // tam recebe da função scahf a quantidade de valores lidos
+   // tam recebe o valor 2, de num1 e num2
    tam = scanf("%d%d", &num1, &num2);
+   // exibe a string e o valor de tam
    printf("Tamanho de retorno de scanf: %d\n", tam);
 
    return 0;
