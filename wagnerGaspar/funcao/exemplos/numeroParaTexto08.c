d18035af6e0e3dfa11515328471856e4dd74267b
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-10 16:59:07 -0300

    Create numeroParaTexto08.c

---
+++ wagnerGaspar/funcao/exemplos/numeroParaTexto08.c
@@ -0,0 +1,13 @@
+/**
+ * Como converter número para texto na
+ * Linguagem C
+ * Como utilizar o valor digitado pelo usuário
+ * na configuração do da função scanf(), para
+ * delimitar a quantidade máxima de caracteres
+ * que podem ser lidos do teclado.
+ * 10/10/2024
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int  
