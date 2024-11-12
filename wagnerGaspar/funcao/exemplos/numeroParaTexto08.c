474e72c7ffb1a739373e2641e3de8084caedf600
PedroSantosFilho <psfilho81@hotmail.com>
2024-11-12 12:09:15 -0300

    Update numeroParaTexto08.c

--- wagnerGaspar/funcao/exemplos/numeroParaTexto08.c
+++ wagnerGaspar/funcao/exemplos/numeroParaTexto08.c
@@ -9,5 +9,32 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int  
+int main()
+{
+   // strings
+   char str1[10] = {"%"}, str2[10];
+
+   // variável
+   int tam = 0;;
+
+   // entrada de dados
+   printf("Digire o tamanho do vetor: ");
+   scanf("%d", &tam);
+   scanf("%c");
+
+   // sprintf(str2, "%d[^\n], tam");
+   snprintf(str2, 10, "%d[^\n], tam");
+   strcat(str1, str2);
+
+   printf("str1 = %s\n", str1);
+   printf("str2 = %s\n", str2);
+
+   char palavra[tam];
+   printf("Digite uma frase: ");
+   scanf(str1, palavra);
+   printf("%s\n", palavra);
+
+   return 0;
+} // end main
