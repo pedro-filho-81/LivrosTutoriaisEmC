d70f9a9fea56999abf69d5db1b54e386e2c2ba68
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-07 17:03:21 -0300

    Update tamanhoDoPonteiro02.c

--- wagnerGaspar/ponteiros/exemplos/tamanhoDoPonteiro02.c
+++ wagnerGaspar/ponteiros/exemplos/tamanhoDoPonteiro02.c
@@ -1,6 +1,7 @@
 /**
  * Qual o tamanho de um ponteiro do tipo 
- * (int, char e float) na linguagem C
+ * (int, char e float) na linguagem C é?
+ * Aula - 185
  */
 #include <stdio.h>
 
@@ -11,6 +12,11 @@ int main()
    float *ponteiro3;
 
    // exibe o tamanho do tipo
+   printf("\nO TAmANHO DO TIPO:");
+   printf("\nO tamanho do tipo int eh: %d byte.\n", sizeof(int));
+   printf("O tamanho do tipo char eh: %d byte.\n", sizeof(char));
+   printf("O tamanho do tipo float eh: %d byte.\n", sizeof(float));
+
    printf("\nO TAmANHO DO TIPO PONTEIRO\n");
    printf("O tamanho do ponteiro do tipo int eh: %d bytes\n", sizeof(ponteiro1));
    printf("O tamanho do ponteiro do tipo char eh: %d bytes\n", sizeof(ponteiro2));
