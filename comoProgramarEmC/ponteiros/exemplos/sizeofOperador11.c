5618fa9eab4b23a8cd88b3b02cec85277f048b56
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-08 16:28:59 -0300

    Update sizeofOperador11.c

--- comoProgramarEmC/ponteiros/exemplos/sizeofOperador11.c
+++ comoProgramarEmC/ponteiros/exemplos/sizeofOperador11.c
@@ -20,15 +20,15 @@ int main()
    int *ptr = array;
 
    printf("\n\tOperador SIZEOF\n");
-   printf("sizeof c = %d byte\tsizeof(char) = %d byte"
-          "\nsizeof s = %d\tsizeof(short) = %d bytes"
-          "\nsizeof i = %d\tsizeof(int) = %d bytes"
-          "\nsizeof l = %d\tsizeof(long) = %d bytes"
-          "\nsizeof f = %d\tsizeof(float) = %d bytes"
-          "\nsizeof d = %d\tsizeof(double) = %d bytes"
-          "\nsizeof ld = %d\tsizeof(long double) = %d bytes"
-          "\nsizeof array = %d bytes"
-          "\nsizeof ptr = %d bytes\n\n",
+   printf("char c;\nsizeof c = %d\tsizeof(char) = %d byte\n"
+          "short s;\nsizeof s = %d\tsizeof(short) = %d bytes\n"
+          "int i;\nsizeof i = %d\tsizeof(int) = %d bytes\n"
+          "long l;\nsizeof l = %d\tsizeof(long) = %d bytes\n"
+          "float f;\nsizeof f = %d\tsizeof(float) = %d bytes\n"
+          "double d;\nsizeof d = %d\tsizeof(double) = %d bytes\n"
+          "long double ld;\nsizeof ld = %d\tsizeof(long double) = %d bytes\n"
+          "int array[20];\nsizeof array = %d bytes\n"
+          "int *ptr = array;\nsizeof ptr = %d bytes\n\n",
           sizeof c, sizeof(char), 
           sizeof s, sizeof(short), 
           sizeof i, sizeof(int),
