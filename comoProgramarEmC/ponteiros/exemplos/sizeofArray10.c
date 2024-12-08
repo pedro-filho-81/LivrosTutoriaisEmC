a7a2784a279fdda079e180924e522c31fddad880
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-08 16:29:06 -0300

    Update sizeofArray10.c

--- comoProgramarEmC/ponteiros/exemplos/sizeofArray10.c
+++ comoProgramarEmC/ponteiros/exemplos/sizeofArray10.c
@@ -14,13 +14,22 @@ int main()
 {
    float array[20]; // cria array
 
-   printf("O numero de byte no array eh: %d"
-            "\nO numero de byte retornado de getSizeof eh: %d\n", sizeof(array), getSizeof(array));
+   printf("\nCria o array float:\nfloat array[20];");
+
+   printf("\nO numero de byte no array eh: %d"
+            
+            "\nVerifica o tamanho do array:\nsizeof(array)\n"
+
+            "\nCria uma funcao:\nsize_t getSizeof( float *ptr);\n"
+
+            "\nO numero de byte retornado de getSizeof eh: %d\n\n", 
+            sizeof(array), getSizeof(array));
 
 } // end main
 
 // function
 size_t getSizeof( float *ptr)
 {
+   // retorna o tamanho do ponteiro
    return sizeof(*ptr);
 } // end getSizeof
