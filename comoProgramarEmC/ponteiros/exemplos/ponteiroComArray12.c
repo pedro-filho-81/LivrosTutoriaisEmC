ec1c03e62591e5cde542ef487f1656c8ad0edec0
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-09 11:55:45 -0300

    Update ponteiroComArray12.c

--- comoProgramarEmC/ponteiros/exemplos/ponteiroComArray12.c
+++ comoProgramarEmC/ponteiros/exemplos/ponteiroComArray12.c
@@ -6,15 +6,26 @@
  */
 #include <stdio.h>
 
-#define SIZE 4
+#define SIZE 4 
 
 int main()
 {
-   // cria vetor
-   int b[] = {10, 20, 30, 40};
-   // cria o ponteiro para vetor
+   // cria um array unidimensional (vetor) do tipo inteiro para 4 elementos
+   int b[SIZE] = {10, 20, 30, 40};
+
+   // exibe
+   printf("\nCria o vetor b:\nint b[SIZE] = {10, 20, 30, 40};\n");
+
+   // cria o ponteiro *bPtr para o array b
+   // todo ponteiro recebe o endereço de uma variável
+   // não é necessário o & comercial para o vetor b
+   // pois o vetor b é um ponteiro
    int *bPtr = b;
-   // contador i
+
+   // exibe
+   printf("Cria o ponteiro *bPtr:\nque recebe o vetor b\nint *bPtr = b;\n");
+
+   // cria contador i
    int i;
    int offset; // contador
 
@@ -38,7 +49,7 @@ int main()
       // exibe array b e seus elementos
       printf("*( b + %d ) = %d\n", offset, *( b + offset ) );
    } // end for
-   
+
    // mostra array b usando bPtr e notação de subscritode array
    printf("\nNotacao de subscrito de ponteiro\n");
 
@@ -59,4 +70,5 @@ int main()
    
    printf("\n\n");
    return 0;
+   
 } // end main
