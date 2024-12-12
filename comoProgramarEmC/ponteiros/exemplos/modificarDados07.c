e3b1793b3caaf2d44bb1087c9669d2cd4d50950d
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-12 17:45:00 -0300

    Update modificarDados07.c

--- comoProgramarEmC/ponteiros/exemplos/modificarDados07.c
+++ comoProgramarEmC/ponteiros/exemplos/modificarDados07.c
@@ -16,12 +16,18 @@ int main()
 
    // cria constante ponteiro
    int *const ptr = &x; // ptr recebe o endereço de x
+   
    printf("\nCria a constante ponteiro:\n\tint *const ptr = &x;\nque recebe o endereco de x.");
+   
    *ptr = 7; // permitido *ptr não é constante
+   
    printf("\nTenta atribuir valor ao ponteiro constante:\n\t*ptr = 7;");
+   
    // ptr = &y; // proibido ptr não pode receber outro endereço
    printf("\nTenta passar o endereco de y ao ponteiro:\n\tptr = &y;\n");
+   
    printf("ptr nao pode receber outro endereÃ§o\n");
+   
    printf("error: assignment of read-only variable 'ptr'");
 
    printf("\n\n");
