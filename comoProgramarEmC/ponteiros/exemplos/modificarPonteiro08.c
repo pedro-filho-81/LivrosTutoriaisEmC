1fecb1c292cbca076cffb4b5ba4eaeb59d891860
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-13 11:46:02 -0300

    Update modificarPonteiro08.c

--- comoProgramarEmC/ponteiros/exemplos/modificarPonteiro08.c
+++ comoProgramarEmC/ponteiros/exemplos/modificarPonteiro08.c
@@ -13,13 +13,32 @@ int main()
    int x = 5;
    int y;
 
+   printf("\nTENTENDO mODIFICAR Um PONTEIRO");
+   printf("\nEm main, Cria variáveis:\n\tint x = 5;\n\tint y;\n");
+
    // cria constante ponteiro
    const int *const ptr = &x; // ptr recebe o endereço de x
-   printf("%d", *ptr);
+   
+   printf("\nEm main, Cria o ponteiro *ptr\nque recebe o endereco de &x:\n\tconst int *const ptr = &x;");
 
-   *ptr = 7; // não permitido *ptr não é constante
-   ptr = &y; // proibido ptr não pode receber outro endereço
+   // "%d", *ptr -  imprime o conteúdo do do ponteiro
+   printf("\nEm main,\n\tprintf(\"%%d\", *ptr);\nImprime o valor de x:  %d", *ptr);
 
+   printf("\n\tprintf(\"%%d\", x);\nImprime o valor de x:  %d", x);
+
+   printf("\n\tprintf(\"%%p\", &x);\nImprime o endereco de x:  %p", &x);
+
+   printf("\n\tprintf(\"%%p\", ptr);\nImprime o endereco de x:  %p", ptr);
+
+   // *ptr = 7; // não permitido *ptr não é constante
+   // ptr = &y; // proibido ptr não pode receber outro endereço
+
+   printf("\nEm main:\n\t*ptr = 7; atribuicao invalida!");
+   printf("\n\t*ptr = &y; Atribuicao invalida!\n");
+   printf("\nO Ponteiro *ptr ja recebeu o endereco de x acima.\n");
+   printf("\nPonteiro so recebe os valores NULL, 0 E Um  ENDERECO");
+
+   printf("\n\n");
    return 0;
 
 } // end main
