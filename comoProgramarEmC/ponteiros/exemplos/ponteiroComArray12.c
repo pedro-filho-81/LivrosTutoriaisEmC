50ab48b498464576597a83faf3704c84052f8a45
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-13 18:05:54 -0300

    Update ponteiroComArray12.c

--- comoProgramarEmC/ponteiros/exemplos/ponteiroComArray12.c
+++ comoProgramarEmC/ponteiros/exemplos/ponteiroComArray12.c
@@ -14,7 +14,7 @@ int main()
    int b[SIZE] = {10, 20, 30, 40};
 
    // exibe
-   printf("\nCria o vetor b:\nint b[SIZE] = {10, 20, 30, 40};\n");
+   printf("\nCria o vetor b:\n\tint b[SIZE] = {10, 20, 30, 40};\n");
 
    // cria o ponteiro *bPtr para o array b
    // todo ponteiro recebe o endereço de uma variável
@@ -23,49 +23,67 @@ int main()
    int *bPtr = b;
 
    // exibe
-   printf("Cria o ponteiro *bPtr:\nque recebe o vetor b\nint *bPtr = b;\n");
+   printf("Cria o ponteiro *bPtr:\nque recebe o vetor b\n\tint *bPtr = b;\n");
 
    // cria contador i
    int i;
    int offset; // contador
+   
+   printf("\nCria as variaveis:\n\tint i;\n\tint offset;");
 
    // mostra array b usnado notação de subscrito de array
-   printf("\nArray b impresso com:\n"
-            "Notacao de subscrito de array.\n");
+   printf("\n\nArray b impresso com:\n"
+            "Notacao de subscrito de array.");
+
+   printf("\nLoop for pelo array:\n\tfor ( i = 0; i < SIZE; i++)");
+
+   printf("\nImrimir o array e os valores:\n\tprintf(\"b[ %%d ] = %%d\", i, b[ i ]);\n");
 
    // loop pelo array b
    for ( i = 0; i < SIZE; i++)
    {
       // exibe array b e seus elementos
-      printf("b[ %d ] = %d\n", i, b[ i ]);
+      printf("\t\tb[ %d ] = %d\n", i, b[ i ]);
    } // end for
 
    printf("\nNotacao de ponteiro/offset onde:\n"
-            "O ponteiro eh o nome do array.\n");
+            "O ponteiro eh o nome do array.");
 
+   printf("\nLoop for pelo array:\n\tfor ( offset = 0; offset < SIZE; offset++)");
+
+   printf("\nImrimir o array e os valores:\n\tprintf(\"*( b + %%d ) = %%d\", offset, *( b + offset ) );\n");
+   
    // loop pelo array b
    for ( offset = 0; offset < SIZE; offset++)
    {
       // exibe array b e seus elementos
-      printf("*( b + %d ) = %d\n", offset, *( b + offset ) );
+      printf("\t\t*( b + %d ) = %d\n", offset, *( b + offset ) );
    } // end for
 
    // mostra array b usando bPtr e notação de subscritode array
-   printf("\nNotacao de subscrito de ponteiro\n");
+   printf("\nNotacao de subscrito de ponteiro");
 
+   printf("\nLoop for pelo array:\n\tfor ( i = 0; i < SIZE; i++)");
+
+   printf("\nImrimir o array e os valores:\n\tprintf(\"bPtr[ %%d ] = %%d\\n\", i, bPtr[i]);\n");
+   
    // loop pelo array
    for ( i = 0; i < SIZE; i++)
    {
-      printf("bPtr[ %d ] = %d\n", i, bPtr[i]);
+      printf("\t\tbPtr[ %d ] = %d\n", i, bPtr[i]);
    } // end for bPtr
  
    // mostra array b usando bPtr e notação de ponteiro/deslocamento
-   printf("\nNotacao de ponteiro/deslocamento\n");
+   printf("\nNotacao de ponteiro/deslocamento");
 
+   printf("\nLoop for pelo array:\n\tfor ( offset = 0; offset < SIZE; offset++)");
+
+   printf("\nImrimir o array e os valores:\n\tprintf(\"*(bPtr + %%d ) = %%d\\n\", offset, *( bPtr + offset));\n");
+   
    // loop pelo array b
    for ( offset = 0; offset < SIZE; offset++)
    {
-      printf("*(bPtr + %d ) = %d\n", offset, *( bPtr + offset));
+      printf("\t\t*(bPtr + %d ) = %d\n", offset, *( bPtr + offset));
    } // end for
    
    printf("\n\n");
