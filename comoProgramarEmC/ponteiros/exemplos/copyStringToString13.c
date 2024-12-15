c3ce63a120ac53af56ef5dd7573f9fb0a6ca2e63
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-15 17:59:43 -0300

    Update copyStringToString13.c

--- comoProgramarEmC/ponteiros/exemplos/copyStringToString13.c
+++ comoProgramarEmC/ponteiros/exemplos/copyStringToString13.c
@@ -15,30 +15,86 @@ int main()
 {
    // cria strings
    char string1[10]; // array para string1
-   char *string2 = "Ola"; // ponteiro para string2
+   printf("\nCria uma string para 10 caracteres:\n\tchar string1[10];");
+
+   printf("\nCria um ponteiro para string:\n\tchar *string2 = \"Estou aprendendo ponteiro.\";");
+   char *string2 = "Estou aprendendo ponteiro."; // ponteiro para string2
+
    char string3[10]; // array para string3
+   printf("\nCria uma string para 10 caracteres:\n\tchar string3[10];");
+
    char string4[] = "Adeus"; // cria um ponteiro para a string4
+   printf("\nCria um ponteiro para uma string:\n\tchar string4[] = \"Adeus\";");
 
    // chama a função
+   printf("\n\nChama o procedimento copy1\nque faz a copia da string2 para string1:\n\tcopy1(string1, string2);");
+   
    copy1(string1, string2);
-   printf("string1 = %s\n", string1);
 
+   printf("\n\nExibe os elementos da string1:"
+            "\nQue sao os caracteres da string2"
+            "\n\tprintf(\"string1 = %%s\", string1);");
+   
+   printf("\n\t\tstring1 = %s\n", string1);
+
+   // chama a função
+   printf("\nChama o procedimento copy2\nque faz a copia da string4 para string3:\n\tcopy2(string3, string4);");
+   
    copy2(string3, string4);
-   printf("string3 = %s\n", string3);
+   
+   printf("\n\nExibe os elementos da string3:"
+            "\nQue sao os caracteres da string4"
+            "\n\tprintf(\"string3 = %%s\", string4);");
+   
+   printf("\n\t\tstring3 = %s\n", string3);
 
    printf("\n\n");
    return 0;
+
 } // end main
 
-// cria o procedimento copy1
+/**
+ * A função copy1 usa a notação de subscrito de array 
+ * para copiar a string de s2 no array de caracteres s1.
+ * A  expressão s1[i] = s2[i] copia um caractere de s2 
+ * em s1.
+ * Deitel, Paul; Deitel, Harvey. C Como Programar 
+ * (Portuguese Edition) (p. 231). Edição do Kindle. 
+ */
 void copy1(char * const s1, const char * const s2)
 {
-   // loop para for
-   for (int i = 0; i < count; i++)
+   /**
+    * loop for para atribuir os valores de s2 para s1
+    * vetor s1[i] recebe os caracteres de s2[i]
+    * enquanto a expressão (s1[i] = s2[i]) for 
+    * diferente de '\0' fim de string
+   */
+   for (int i = 0; (s1[i] = s2[i]) != '\0'; i++)
    {
-      /* code */
-   }
-   
+
+   } // end for
 } // end copy1
 
-void copy2(char *s1, const char *s2);
+/**
+ * O procedimento copy2 usa ponteiros e aritmética de 
+ * ponteiro para copiar a string de s2 no array de 
+ * caracteres s1.
+ * Deitel, Paul; Deitel, Harvey. C Como Programar 
+ * (Portuguese Edition) (p. 231). Edição do Kindle. 
+ */
+void copy2(char *s1, const char *s2)
+{
+   /**
+    * a expressão (*s1 = *s2) realiza a operação de 
+    * cópia. Após a atribuição na condição, 
+    * os ponteiros são incrementados para que apontem 
+    * para  o próximo elemento do array s1 e o próximo 
+    * caractere de s2, respectivamente. 
+    * Deitel, Paul; Deitel, Harvey. C Como Programar 
+    * (Portuguese Edition) (p. 231). Edição do Kindle. 
+    */
+   for ( ; (*s1 = *s2); s1++, s2++)
+   {
+
+   } // end for
+} // end copy2
