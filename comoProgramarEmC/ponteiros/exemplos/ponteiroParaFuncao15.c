698580250ec03b21490b35357629b58729fffaf8
PedroSantosFilho <psfilho81@hotmail.com>
2024-12-16 12:11:17 -0300

    Update ponteiroParaFuncao15.c

--- comoProgramarEmC/ponteiros/exemplos/ponteiroParaFuncao15.c
+++ comoProgramarEmC/ponteiros/exemplos/ponteiroParaFuncao15.c
@@ -10,72 +10,98 @@
 #define SIZE 10
 
 // PROTÓTIPOs
-void bobble(int work[], const int size, int (*compare)(int a int b));
+void bubble(int work[], const int size, int (*compare)(int a, int b));
 int ascending(int a, int b);
 int descending(int a, int b); 
 
 int main()
 {
    // variaveis
-   int order; // 1 ordem crescente, 2 ordem decrescente
+   int order = 0; // 1 ordem crescente, 2 ordem decrescente
    // vetor
-   int counter; // contador
+   int counter = 0; // contador
    
+   // inicializa array
    int vetor[SIZE] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
 
-   // variável
-   int i = 0; // contador
+   // entrada de dados
+   printf("\nDigite 1 para classificar em ordem crescente\n"
+            "Digite 2 para classificar em ordem decrescente: ");
+   scanf("%d", &order);
 
-   printf("Itens de dados na ordem original:\n");
+   printf("\nItens de dados na ordem original:\n");
 
    // loop for para exibir vetor
-   for ( i = 0; i < SIZE; i++)
-   {
-      printf("%4d", vetor[i]);
+   for ( counter = 0; counter < SIZE; counter++)
+      {
+      printf("%4d", vetor[counter]);
    } // end for
 
-   // chama a função para ordenar o vetor
-   bobbleSort( vetor, SIZE);
+   /**
+    * Classifica array em ordem crescente: passa a 
+    * função crescente como um argumento para espe-
+    * cificar classificação crescente
+    */
 
-   printf("\nItens de dado em ordem crescente:\n");
-
-   // loop for para exibir o vetor ordenado
-   for ( i = 0; i < SIZE; i++)
+   if (order == 1)
    {
-      printf("%4d", vetor[i]);
+      bubble(vetor, SIZE, ascending);   
+      printf("\nItens de dado em ordem crescente:\n");
+   } // end if
+   else { // passa para a função de desending
+      bubble(vetor, SIZE, descending);      
+      printf("\nItens de dado em ordem decrescente:\n");
+   } // end else
+   
+   // loop for para exibir o vetor ordenado
+   for ( counter = 0; counter < SIZE; counter++)
+   {
+      printf("%4d", vetor[counter]);
    } // end for
    
-   printf("\n");
-
+   printf("\n\n");
    return 0;
 
 }// end main
 
 // function
-void bobbleSort(int *const vetor[], const int size)
+void bubble(int work[], const int size, int (*compare)(int a, int b))
 {
-   void swap(int *element1Ptr, int *element2Ptr);
-
    // variável
-   int pass; // passadas
-   int j; // 
+   int passada = 0; // passadas
+   int counter = 0; // 
 
-   for ( pass = 0; pass < size - 1; pass++)
+   // protótipo
+   void inverte(int *element1Ptr, int *element2Ptr);
+
+   for ( passada = 1; passada < size; passada++)
    {
-      for ( j = 0; j < size; j++)
-      {
-         if (vetor[j] > vetor[j + 1])
+      for ( counter = 0; counter < size - 1; counter++)
+      {  
+         if ((work[counter] > work[counter + 1]))
          {
-            swap(&vetor[j], &vetor[j + 1]);
+            inverte(&work[counter], &work[counter + 1]);
          } // end if
       } // end for j
    } // end for pass   
-} // end function
+} // end function bobble
 
 // function swap
-void swap(int *element1Ptr, int *element2Ptr)
+void inverte(int *element1Ptr, int *element2Ptr)
 {
    int hold = *element1Ptr;
    *element1Ptr = *element2Ptr;
    *element2Ptr = hold;
 } // end swap
+
+// function ascending
+int ascending(int a, int b)
+{
+   return b < a;
+} // end ascending
+
+// finction descending  
+int descending(int a, int b)
+{
+   return b > a;
+} // end descending
