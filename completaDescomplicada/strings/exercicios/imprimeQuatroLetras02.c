b4cab866681d88c0ba6dd13486eaddd37c997b00
PedroSantosFilho <psfilho81@hotmail.com>
2025-01-11 16:49:01 -0300

    Create imprimeQuatroLetras02.c

---
+++ completaDescomplicada/strings/exercicios/imprimeQuatroLetras02.c
@@ -0,0 +1,24 @@
+/**
+ * 2) Faça um programa que leia uma string e imprima as 
+ * quatro primeiras letras dela.
+ * Backes, André. Linguagem C - Completa e Descomplicada 
+ * (Portuguese Edition) (p. 139). 
+ * GEN LTC. Edição do Kindle. 
+ */
+#include <stdio.h>
+
+int main()
+{
+   // string
+   char texto[50];
+
+   // entrada de dados
+   printf("\nDigite uma palavra ou um texto: ");
+   fgets(texto, 5, stdin);
+
+   // exibe resultado
+   printf("\nVoce digitou: %s", texto);
+
+   printf("\n\n");
+   return 0;
+} // end main
