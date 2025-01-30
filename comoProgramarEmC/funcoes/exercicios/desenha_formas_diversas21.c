a098cf5ee7175a21282abf62b1ffe4556afa15fe
PedroSantosFilho <psfilho81@hotmail.com>
2025-01-30 11:50:22 -0300

    Create desenha_formas_diversas21.c

---
+++ comoProgramarEmC/funcoes/exercicios/desenha_formas_diversas21.c
@@ -0,0 +1,9 @@
+/**
+ * Projeto: desenhando formas com caracteres.  Use 
+ * técnicas semelhantes às que foram desenvolvidas  
+ * nos exercícios 5.19 a 5.20 para produzir um 
+ * programa  que represente graficamente uma grande 
+ * variedade  de formas. 
+ * Deitel, Paul; Deitel, Harvey. C Como Programar 
+ * (Portuguese Edition) (p. 154). Edição do Kindle. 
+ */
