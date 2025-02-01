6f52be839ddda8e96d6e75e71175d162db689a2d
PedroSantosFilho <psfilho81@hotmail.com>
2025-02-01 11:58:29 -0300

    Create frequencia_notas_studant05.c

---
+++ comoProgramarEmC/arrays/exemplos/frequencia_notas_studant05.c
@@ -0,0 +1,8 @@
+/**
+ * fig06_05.c  
+ * Analyzing a student poll. 
+ * Deitel, Paul; Deitel, Harvey. C How to Program: With 
+ * Case Studies in Applications and Systems Programming, 
+ * Global Edition (p. 305). Pearson Education. 
+ * Edição do Kindle. 
+ */
