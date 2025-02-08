a093464380326a586837393cf7b4c321ecfbc3de
PedroSantosFilho <psfilho81@hotmail.com>
2025-02-08 11:38:42 -0300

    Create exer05.c

---
+++ comoProgramarEmC/ch07_ponteiros/exercicios/exer05.c
@@ -0,0 +1,41 @@
+/**
+ * 7.5 Faça cada um dos seguintes:
+ * a) Escreva o cabeçalho da função para uma troca de 
+ * funções que recebe dois ponteiros para números de 
+ * ponto flutuante x e y como parâmetros e não retorna 
+ * um valor.
+ * b) Escreva o protótipo da função para a função na 
+ * parte (a).
+ * c) Escreva o cabeçalho da função para uma função 
+ * assess que retorna um inteiro e que recebe como 
+ * parâmetros o inteiro x e um ponteiro para a função 
+ * poly, que representa uma função que recebe um 
+ * parâmetro inteiro e retorna um inteiro.
+ * d) Escreva o protótipo da função para a função na 
+ * parte (c).
+ * Deitel, Paul; Deitel, Harvey. C How to Program: 
+ * With Case Studies in Applications and Systems 
+ * Programming, Global Edition (p. 407). Pearson 
+ * Education. Edição do Kindle.
+ */
+#include <stdio.h>
+
+int main()
+{
+   puts("\nPrototipos de funcao:\n");
+   puts("a) Escreva o cabeçalho da função para uma troca de" 
+         "\nfunções que recebe dois ponteiros para números de "
+         "\num valor.");
+   puts("\tvoid exchange(int *xPtr, int yPtr);");
+
+   puts("\nb) Escreva o protótipo da função para a função na parte (a).");
+   puts("\tvoid exchange(int *xPtr, int yPtr);");
+
+   puts("\nc) Escreva o cabeçalho da função para uma função" 
+         "\nassess que retorna um inteiro e que recebe como "
+         "\nparâmetros o inteiro x e um ponteiro para a função "
+         "\npoly, que representa uma função que recebe um "
+         "\nparâmetro inteiro e retorna um inteiro.");
+   puts("\n\t");
+
+} // end main
